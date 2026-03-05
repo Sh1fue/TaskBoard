a280f1eb2faa707047511160426e4bbc75ceb0ef
Вадим <vadim@MacBook-Air-Vadim.local>
2026-03-05 14:13:34 +0300

    user,task

---
+++ cmd/server/internal/domain/user.go
@@ -0,0 +1,14 @@
+package domain
+
+import (
+	"time"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID        int       `json:"id"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module trello_parody
+
+go 1.25.7
+
+require golang.org/x/crypto v0.48.0 // indirect
